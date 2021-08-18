f47bd9e4e695682d3460ede5cfac3c40a509845e
kenezeoke <kenezeoke@yahoo.com>
2021-08-18 04:50:15 -0400

    Added table to frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]# just only shows the show action route
+  resources :articles, only: [:show, :index]
 end
