22b4a1bc7e64362159982529ad272916d55f9d6b
kenezeoke <kenezeoke@yahoo.com>
2021-08-18 18:47:43 -0400

    form added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,14 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
-    
 
+
+  end
+  def new
+  end
+
+  def create
+    render plain: params[:article]
   end
 
 
