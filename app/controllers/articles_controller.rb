f16f08fe02db28c895ca8ccdad318cbcde45c625
kenezeoke <kenezeoke@yahoo.com>
2021-08-19 00:54:16 -0400

    refactored code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,19 @@
 class ArticlesController < ApplicationController
+  # runs the function set_article first for the show, edit, update,destroy function
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-
-    @article = Article.find(params[:id])
   end
 
   def index
     @articles = Article.all
-
-
   end
+  
   def new
     @article = Article.new
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     # this requires the article key and permits only the use of title and description
     if @article.save # save the created article
       #This gives a notice
@@ -26,12 +25,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated succesfully"
       redirect_to @article
     else
@@ -41,9 +38,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
+  private #any code below private is only available to this controller
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
